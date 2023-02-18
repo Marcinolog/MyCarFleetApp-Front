35ef05b9d3979d4fb7f5aedc0c93d0ed1176c579
Marcin <116343437+Marcinolog@users.noreply.github.com>
2023-02-18 23:40:14 +0100

    Add buttons to header, and add components rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,60 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import {YourCarComponent} from "./components/YourCars/YourCars";
+import {DelegationsComponent} from "./components/Delegations/Delegations";
+import {PrivateUseComponent} from "./components/PrivateUse/PrivateUse";
+import {ReportDamageComponent} from "./components/ReportDamage/ReportDamage";
 
-function App() {
-  return (
 
-  );
-}
+export function App() {
+    const [showYourCarComponent, setShowYourCarComponent] = useState(false);
+    const [showDelegationsComponent, setShowDelegationsComponent] = useState(false);
+    const [showPrivateUseComponent, setShowPrivateUseComponent] = useState(false);
+    const [showReportDamageComponent, setShowReportDamageComponent] = useState(false);
 
-export default App;
+    const handleYourCarComponentClick = () => {
+        setShowYourCarComponent(true);
+        setShowDelegationsComponent(false);
+        setShowPrivateUseComponent(false);
+        setShowReportDamageComponent(false);
+    };
+
+    const handleDelegationsComponentClick = () => {
+        setShowYourCarComponent(false);
+        setShowDelegationsComponent(true);
+        setShowPrivateUseComponent(false);
+        setShowReportDamageComponent(false);
+    };
+    const handlePrivateUseComponentClick = () => {
+        setShowYourCarComponent(false);
+        setShowDelegationsComponent(false);
+        setShowPrivateUseComponent(true);
+        setShowReportDamageComponent(false);
+    };
+
+    const handleReportDamageComponentClick = () => {
+        setShowYourCarComponent(false);
+        setShowDelegationsComponent(false);
+        setShowPrivateUseComponent(false);
+        setShowReportDamageComponent(true);
+    };
+
+
+    return (
+        <>
+            <header>
+                <h1>Welcome to MyCarFleet</h1>
+                <button className="your-cars" onClick={handleYourCarComponentClick}>Add new car</button>
+                <button className="delegations" onClick={handleDelegationsComponentClick}>Add new delegation</button>
+                <button className="private-use" onClick={handlePrivateUseComponentClick}>Add new private use</button>
+                <button className="report-damage" onClick={handleReportDamageComponentClick}>Report damage</button>
+            </header>
+            {showYourCarComponent && <YourCarComponent/>}
+            {showDelegationsComponent && <DelegationsComponent/>}
+            {showPrivateUseComponent && <PrivateUseComponent/>}
+            {showReportDamageComponent && <ReportDamageComponent/>}
+        </>
+    )
+};
+
+
